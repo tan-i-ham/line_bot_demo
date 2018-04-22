64cabfd3a97336657dcc449608f7ff91a48ba00e
tp6hannah <hannah51125@gmail.com>
2018-04-22 22:27:45 +0800

    change port

--- app.py
+++ app.py
@@ -95,15 +95,21 @@ def handle_text_message(event):
             event.reply_token,
             message)
 
+# if __name__ == "__main__":
+#     arg_parser = ArgumentParser(
+#         usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+#     )
+#     arg_parser.add_argument('-p', '--port', type=int, default=5000, help='port')
+#     arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+#     options = arg_parser.parse_args()
+
+#     # create tmp dir for download content
+#     # make_static_tmp_dir()
+
+#     app.run(debug=options.debug, port=options.port)
+
+
 if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
-    # create tmp dir for download content
-    # make_static_tmp_dir()
-
-    app.run(debug=options.debug, port=options.port)
