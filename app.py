14958353424a01162378d6569c7d6bd2ec0bfa9c
tp6hannah <hannah51125@gmail.com>
2018-04-22 22:09:35 +0800

    copy line demo

--- app.py
+++ app.py
@@ -44,42 +44,160 @@ def callback():
     return 'OK'
 
 
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     # message = TextSendMessage(text=event.message.text)
+#     # line_bot_api.reply_message(
+#     #     event.reply_token,
+#     #     message)
+#     message = TemplateSendMessage(
+#         alt_text='Buttons template',
+#         template=ButtonsTemplate(
+#             thumbnail_image_url='https://example.com/image.jpg',
+#             title='Who is Yi-Han Chen?',
+#             text='Student from NTUST, Taiwan . Familiar with Python and Java',
+#             actions=[
+#                 PostbackTemplateAction(
+#                     label='postback',
+#                     text="you just sned "+k,
+#                     data='action=buy&itemid=1'
+#                 ),
+#                 MessageTemplateAction(
+#                     label='What is my side project recently?',
+#                     text='side project'
+#                 ),
+#                 URITemplateAction(
+#                     label='My Linkedin',
+#                     uri='https://www.linkedin.com/in/hannah-chen-326918101/'
+#                 )
+#             ]
+#         )
+#     )
+#     line_bot_api.reply_message(event.reply_token, message)
+
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    # message = TextSendMessage(text=event.message.text)
-    # line_bot_api.reply_message(
-    #     event.reply_token,
-    #     message)
-    k = event.message.text
-    if k =='test':
-        message = TemplateSendMessage(
-            alt_text='Buttons template',
-            template=ButtonsTemplate(
-                thumbnail_image_url='https://example.com/image.jpg',
-                title='Who is Yi-Han Chen?',
-                text='Student from NTUST, Taiwan . Familiar with Python and Java',
-                actions=[
-                    PostbackTemplateAction(
-                        label='postback',
-                        text="you just sned ",
-                        data='action=buy&itemid=1'
+def handle_text_message(event):
+    text = event.message.text
+
+    if text == 'profile':
+        if isinstance(event.source, SourceUser):
+            profile = line_bot_api.get_profile(event.source.user_id)
+            line_bot_api.reply_message(
+                event.reply_token, [
+                    TextSendMessage(
+                        text='Display name: ' + profile.display_name
                     ),
-                    MessageTemplateAction(
-                        label='What is my side project recently?',
-                        text='side project'
-                    ),
-                    URITemplateAction(
-                        label='My Linkedin',
-                        uri='https://www.linkedin.com/in/hannah-chen-326918101/'
+                    TextSendMessage(
+                        text='Status message: ' + profile.status_message
                     )
                 ]
             )
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextMessage(text="Bot can't use profile API without user ID"))
+    elif text == 'bye':
+        if isinstance(event.source, SourceGroup):
+            line_bot_api.reply_message(
+                event.reply_token, TextMessage(text='Leaving group'))
+            line_bot_api.leave_group(event.source.group_id)
+        elif isinstance(event.source, SourceRoom):
+            line_bot_api.reply_message(
+                event.reply_token, TextMessage(text='Leaving group'))
+            line_bot_api.leave_room(event.source.room_id)
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextMessage(text="Bot can't leave from 1:1 chat"))
+    elif text == 'confirm':
+        confirm_template = ConfirmTemplate(text='Do it?', actions=[
+            MessageTemplateAction(label='Yes', text='Yes!'),
+            MessageTemplateAction(label='No', text='No!'),
+        ])
+        template_message = TemplateSendMessage(
+            alt_text='Confirm alt text', template=confirm_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+    elif text == 'buttons':
+        buttons_template = ButtonsTemplate(
+            title='My buttons sample', text='Hello, my buttons', actions=[
+                URITemplateAction(
+                    label='Go to line.me', uri='https://line.me'),
+                PostbackTemplateAction(label='ping', data='ping'),
+                PostbackTemplateAction(
+                    label='ping with text', data='ping',
+                    text='ping'),
+                MessageTemplateAction(label='Translate Rice', text='米')
+            ])
+        template_message = TemplateSendMessage(
+            alt_text='Buttons alt text', template=buttons_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+    elif text == 'carousel':
+        carousel_template = CarouselTemplate(columns=[
+            CarouselColumn(text='hoge1', title='fuga1', actions=[
+                URITemplateAction(
+                    label='Go to line.me', uri='https://line.me'),
+                PostbackTemplateAction(label='ping', data='ping')
+            ]),
+            CarouselColumn(text='hoge2', title='fuga2', actions=[
+                PostbackTemplateAction(
+                    label='ping with text', data='ping',
+                    text='ping'),
+                MessageTemplateAction(label='Translate Rice', text='米')
+            ]),
+        ])
+        template_message = TemplateSendMessage(
+            alt_text='Carousel alt text', template=carousel_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+    elif text == 'image_carousel':
+        image_carousel_template = ImageCarouselTemplate(columns=[
+            ImageCarouselColumn(image_url='https://via.placeholder.com/1024x1024',
+                                action=DatetimePickerTemplateAction(label='datetime',
+                                                                    data='datetime_postback',
+                                                                    mode='datetime')),
+            ImageCarouselColumn(image_url='https://via.placeholder.com/1024x1024',
+                                action=DatetimePickerTemplateAction(label='date',
+                                                                    data='date_postback',
+                                                                    mode='date'))
+        ])
+        template_message = TemplateSendMessage(
+            alt_text='ImageCarousel alt text', template=image_carousel_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+    elif text == 'imagemap':
+        pass
+    else:
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=event.message.text))
+
+
+@handler.add(MessageEvent, message=LocationMessage)
+def handle_location_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        LocationSendMessage(
+            title=event.message.title, address=event.message.address,
+            latitude=event.message.latitude, longitude=event.message.longitude
         )
-        line_bot_api.reply_message(event.reply_token, message)
+    )
 
 
+@handler.add(MessageEvent, message=StickerMessage)
+def handle_sticker_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        StickerSendMessage(
+            package_id=event.message.package_id,
+            sticker_id=event.message.sticker_id)
+    )
 
-import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    options = arg_parser.parse_args()
+
+    # create tmp dir for download content
+    make_static_tmp_dir()
+
+    app.run(debug=options.debug, port=options.port)
